6b6b466ed752813fce6d06ca4af7785bc3d4a6cf
ACCRETIO\johnrendell.canteras <johnrendell.canteras@wealthylifestyle.com.ph>
2025-04-30 15:12:59 +0800

    first commit

---
+++ lib/widgets/expenses_list.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense_model.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+  });
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (context, index) => Text(
+        expenses[index].title, 
+      ),
+    );
+  }
+}
